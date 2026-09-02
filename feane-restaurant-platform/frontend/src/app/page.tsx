3ad999519605545e7e07b23d574b8b417c1b44b9
robel <robelyinager12@gmail.com>
2026-09-02 10:30:02 +0300

    Assemble complete homepage with all sections in order

--- feane-restaurant-platform/frontend/src/app/page.tsx
+++ feane-restaurant-platform/frontend/src/app/page.tsx
@@ -1,13 +1,32 @@
 // File: frontend/src/app/page.tsx
 import { Navbar } from '../components/layout/Navbar';
+import { Footer } from '../components/layout/Footer';
 import { Hero } from '../components/home/Hero';
+import { RestaurantIntroduction } from '../components/home/RestaurantIntroduction';
+import { FeaturedMenu } from '../components/home/FeaturedMenu';
+import { RestaurantExperience } from '../components/home/RestaurantExperience';
+import { SpecialOffers } from '../components/home/SpecialOffers';
+import { ReservationSection } from '../components/home/ReservationSection';
+import { Gallery } from '../components/home/Gallery';
+import { CustomerReviews } from '../components/home/CustomerReviews';
+import { LocationHours } from '../components/home/LocationHours';
+import { Newsletter } from '../components/home/Newsletter';
 
 export default function HomePage() {
   return (
     <main>
       <Navbar />
       <Hero />
-      {/* Featured menu, about teaser, testimonials sections come next */}
+      <RestaurantIntroduction />
+      <FeaturedMenu />
+      <RestaurantExperience />
+      <SpecialOffers />
+      <ReservationSection />
+      <Gallery />
+      <CustomerReviews />
+      <LocationHours />
+      <Newsletter />
+      <Footer />
     </main>
   );
 }
