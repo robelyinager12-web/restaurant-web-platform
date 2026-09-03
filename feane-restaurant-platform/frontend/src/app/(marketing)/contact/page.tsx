567766afbe6720db5575a69b186e0e76944fd5ec
robel <robelyinager12@gmail.com>
2026-09-03 14:41:43 +0300

    Re-add contact page using new ContactSplit component

--- feane-restaurant-platform/frontend/src/app/(marketing)/contact/page.tsx
+++ feane-restaurant-platform/frontend/src/app/(marketing)/contact/page.tsx
@@ -1,33 +1,15 @@
 // File: frontend/src/app/(marketing)/contact/page.tsx
 import { Navbar } from '../../../components/layout/Navbar';
 import { Footer } from '../../../components/layout/Footer';
-import { ContactHero } from '../../../components/contact/ContactHero';
-import { ContactInfo } from '../../../components/contact/ContactInfo';
-import { ContactHours } from '../../../components/contact/ContactHours';
-import { ContactFormSection } from '../../../components/contact/ContactFormSection';
-import { ContactMap } from '../../../components/contact/ContactMap';
-import { ReservationsCTA } from '../../../components/contact/ReservationsCTA';
-import { PrivateEvents } from '../../../components/contact/PrivateEvents';
-import { SocialMedia } from '../../../components/contact/SocialMedia';
-import { ContactFAQ } from '../../../components/contact/ContactFAQ';
-import { ContactFinalCTA } from '../../../components/contact/ContactFinalCTA';
+import { ContactSplit } from '../../../components/contact/ContactSplit';
 
 export default function ContactPage() {
   return (
     <main>
       <Navbar />
-      <ContactHero />
-      <ContactInfo />
-      <ContactHours />
-      <div id="contact-form">
-        <ContactFormSection />
+      <div className="pt-16">
+        <ContactSplit />
       </div>
-      <ContactMap />
-      <ReservationsCTA />
-      <PrivateEvents />
-      <SocialMedia />
-      <ContactFAQ />
-      <ContactFinalCTA />
       <Footer />
     </main>
   );
